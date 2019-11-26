34c6bc882c4d73cf1c5923844b2e92ac8ea2621f
RossKass <rosskass8@gmail.com>
2019-11-26 17:39:51 +0200

    update About(images)

--- js/script.js
+++ js/script.js
@@ -10,18 +10,8 @@ $(document).ready(function () {
     .setClassToggle('.block-top', 'top-line-white')
     .addTo(controllerMenu);  
   }
-  // menu click humburger
-  $(".hamburger-block").click( function() {
-    $(".block-mnu").removeClass("mnu-hidden");
-  });
-  
-  $(".mnu-closed").click( function() {
-    $(".block-mnu").addClass("mnu-hidden");
-  });
+  eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('d $6=$(\'c, g\');$(\'.h-i a[4*="#"], .7 a[4*="#"]\').3(1(e){e.9();$6.b({f:$($.n(p,\'4\')).j().q},o)});$(".k-2").3(1(){$(".2-0").l("0-5")});$(".0-m").3(1(){$(".2-0").8("0-5")});$(".7 a").3(1(){$(".2-0").8("0-5")});',27,27,'mnu|function|block|click|href|hidden|page|menu|addClass|preventDefault||animate|html|var||scrollTop|body|nmu|list|offset|hamburger|removeClass|closed|attr|400|this|top'.split('|'),0,{}))
 
-  $(".menu a").click( function() {
-    $(".block-mnu").addClass("mnu-hidden");
-  });
   $(".scroll-left").each(function(){var controller1=new ScrollMagic.Controller();var textAnimation_1=new TimelineMax().fromTo(this,1,{x:"0%"},{x:"250%",ease:Linear.easeNone})
   new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_1).addTo(controller1)});$(".scroll-right").each(function(){var controller2=new ScrollMagic.Controller();var textAnimation_2=new TimelineMax().fromTo(this,1,{x:"0%"},{x:"-200%",ease:Linear.easeNone})
   new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_2).addTo(controller2)});$(function(){var controller3=new ScrollMagic.Controller();var textAnimation_3=new TimelineMax().fromTo(".scroll-right-contact",1,{x:"120%"},{x:"0%",ease:Linear.easeNone})
