a67ed96255528a8ff589fe9eff8136324d28c60e
RossKass <rosskass8@gmail.com>
2019-11-26 14:11:45 +0200

    update menu(line)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 /*jshint esversion: 6 */
 $(document).ready(function () {
 
-	eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('a($("b").9("d")){8 6=2 5.7();2 5.c({h:".e-j",i:\'0.1\'}).g(\'.3-4\',\'3-4-k\').f(6)}',21,21,'||new|top|line|ScrollMagic|controllerMenu|Controller|var|hasClass|if|body|Scene|home|scroll|addTo|setClassToggle|triggerElement|triggerHook|trigger|white'.split('|'),0,{}))
+	if ($("body").hasClass("home")) {
+    var controllerMenu = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({
+      triggerElement: ".scroll-trigger",
+      triggerHook: '0.1'
+    })
+    .setClassToggle('.block-top', 'top-line-white')
+    .addTo(controllerMenu);  
+  }
+  // menu click humburger
+  $(".hamburger-block").click( function() {
+    $(".block-mnu").removeClass("mnu-hidden");
+  });
+  
+  $(".mnu-closed").click( function() {
+    $(".block-mnu").addClass("mnu-hidden");
+  });
+
+  $(".menu a").click( function() {
+    $(".block-mnu").addClass("mnu-hidden");
+  });
   $(".scroll-left").each(function(){var controller1=new ScrollMagic.Controller();var textAnimation_1=new TimelineMax().fromTo(this,1,{x:"0%"},{x:"250%",ease:Linear.easeNone})
   new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_1).addTo(controller1)});$(".scroll-right").each(function(){var controller2=new ScrollMagic.Controller();var textAnimation_2=new TimelineMax().fromTo(this,1,{x:"0%"},{x:"-200%",ease:Linear.easeNone})
   new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_2).addTo(controller2)});$(function(){var controller3=new ScrollMagic.Controller();var textAnimation_3=new TimelineMax().fromTo(".scroll-right-contact",1,{x:"120%"},{x:"0%",ease:Linear.easeNone})
   if(window.innerWidth>767){new ScrollMagic.Scene({triggerElement:"#contact",triggerHook:0.7,duration:"45%"}).setTween(textAnimation_3).addTo(controller3)}else{new ScrollMagic.Scene({triggerElement:"#contact",triggerHook:0.7,duration:"35%"}).setTween(textAnimation_3).addTo(controller3)}});
-  eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('T $y=$(\'18, 17\');$(\'.13-12 a[p*="#"], .f a[p*="#"]\').d(h(e){e.U();$y.11({S:$($.V(3,\'p\')).C().16},14)});$(".15-g").d(h(){$(".g-9").Z("9-o")});$(".9-Y").d(h(){$(".g-9").u("9-o")});$(".f a").d(h(){$(".g-9").u("9-o")});{A w=B.D(W.E(\'.f a\'));R s{X(6){3.5={};3.5.6=6;3.5.n=6.1a(\'.f a G\');1k(3.5.n);3.5.b=B.D(3.5.n.E(\'G\'));3.F()}F(){A 4=1j;3.q=()=>3.v=1n(()=>{3.m=1p;k.I(3.5.b);k({x:3.5.b,H:(t,i)=>i*P,Q:[{8:(t,i)=>i%2===0?10:-10,4:4,7:\'O\'},{8:(t,i)=>i%2===0?[-10,0]:[10,0],4:4+M,7:\'N\',L:K}],J:[{8:0,4:4,7:\'j\'},{8:1,4:4,7:\'j\'}]})},1o);3.r=()=>{1q(3.v);1i(!3.m)19;3.m=1c;k.I(3.5.b);k({x:3.5.b,H:(t,i,l)=>(l-i-1)*P,Q:[{8:(t,i)=>i%2===0?-10:10,4:4,7:\'O\'},{8:(t,i)=>i%2===0?[10,0]:[-10,0],4:4+M,7:\'N\',L:K}],J:[{8:0,4:4,7:\'j\'},{8:1,4:4,7:\'j\'}]})};3.5.6.c(\'1f\',3.q);3.5.6.c(\'1m\',3.q);3.5.6.c(\'1g\',3.r);3.5.6.c(\'1e\',3.r)}};w.1d(z=>1b s(z))};1h.1l({C:10});',62,89,'|||this|duration|DOM|el|easing|value|mnu||nameLetters|addEventListener|click||menu|block|function||linear|anime||isActive|name|hidden|href|mouseenterFn|mouseleaveFn|Item||addClass|mouseTimeout|items|targets|page|item|const|Array|offset|from|querySelectorAll|initEvents|span|delay|remove|opacity|600|elasticity|700|easeOutElastic|easeInSine|20|translateY|class|scrollTop|var|preventDefault|attr|document|constructor|closed|removeClass||animate|list|nmu|400|hamburger|top|body|html|return|querySelector|new|false|forEach|touchend|mouseenter|mouseleave|AOS|if|150|charming|init|touchstart|setTimeout|50|true|clearTimeout'.split('|'),0,{}))
-
+  
+  AOS.init({
+    offset: 10
+  });
 });
