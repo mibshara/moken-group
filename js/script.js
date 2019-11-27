f1e46e70a90f405e7ea221c9f13a8b2d729e8138
RossKass <rosskass8@gmail.com>
2019-11-27 20:07:34 +0200

    update footer

--- js/script.js
+++ js/script.js
@@ -14,9 +14,8 @@ $(document).ready(function () {
 
   $(".scroll-left").each(function(){var controller1=new ScrollMagic.Controller();var textAnimation_1=new TimelineMax().fromTo(this,1,{x:"0%"},{x:"250%",ease:Linear.easeNone})
   new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_1).addTo(controller1)});$(".scroll-right").each(function(){var controller2=new ScrollMagic.Controller();var textAnimation_2=new TimelineMax().fromTo(this,1,{x:"0%"},{x:"-200%",ease:Linear.easeNone})
-  new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_2).addTo(controller2)});$(function(){var controller3=new ScrollMagic.Controller();var textAnimation_3=new TimelineMax().fromTo(".scroll-right-contact",1,{x:"120%"},{x:"0%",ease:Linear.easeNone})
-  if(window.innerWidth>767){new ScrollMagic.Scene({triggerElement:"#contact",triggerHook:0.7,duration:"45%"}).setTween(textAnimation_3).addTo(controller3)}else{new ScrollMagic.Scene({triggerElement:"#contact",triggerHook:0.7,duration:"35%"}).setTween(textAnimation_3).addTo(controller3)}});
-  
+  new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_2).addTo(controller2)});
+    
   AOS.init({
     offset: 10
   });
