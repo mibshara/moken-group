4f0009ab8bb8e11882e046cce6d604ff9b49687c
RossKass <rosskass8@gmail.com>
2019-12-06 11:16:15 +0200

    new Careers page

--- js/script.js
+++ js/script.js
@@ -15,7 +15,9 @@ $(document).ready(function () {
   $(".scroll-left").each(function(){var controller1=new ScrollMagic.Controller();var textAnimation_1=new TimelineMax().fromTo(this,1,{x:"0%"},{x:"250%",ease:Linear.easeNone})
   new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_1).addTo(controller1)});$(".scroll-right").each(function(){var controller2=new ScrollMagic.Controller();var textAnimation_2=new TimelineMax().fromTo(this,1,{x:"0%"},{x:"-200%",ease:Linear.easeNone})
   new ScrollMagic.Scene({triggerElement:this,triggerHook:0.5,duration:"200%"}).setTween(textAnimation_2).addTo(controller2)});
-    
+  
+  eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(".5-3-2").a(9(){$(".5-3-2").7("1");$(8).6("1");$(".e-d").6("4");f 0=$(8).c("b-0");$("."+0).7("4")});',16,16,'id|active|title|head|hidden|positions|addClass|removeClass|this|function|click|data|attr|content|ps|let'.split('|'),0,{}))
+
   AOS.init({
     offset: 10
   });
